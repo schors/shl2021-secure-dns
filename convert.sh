95619dfdefb769bceef887127e4c1fd316d9182f
Phil Kulin <schors@gmail.com>
2021-09-19 02:59:57 +0300

    some etap

--- convert.sh
+++ convert.sh
@@ -6,8 +6,9 @@ if [ -x "/usr/bin/convert" ]; then
                 mkdir -p slide
         fi
         cd slide
+        rm -f *.png
         convert -density 600 ../dns-security.pdf -strip -resize 600  PNG8:slide-%02d.png
-        convert -layers OptimizePlus -delay 200 slide-00.png -delay 125 slide-0[1234567].png -loop 0 ../dns-security.gif
+        convert -layers OptimizePlus -delay 200 slide-00.png -delay 125 slide-0[0123456789].png -delay 125 slide-[123456789][0123456789].png -loop 0 ../dns-security.gif
 else
         echo "You must install ImageMagick!"
 fi
